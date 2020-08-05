32b4d6e85dffa084b49bff193d0cb746a318e0fa
TienNguyen <ngoctiennguyen155@gmail.com>
2020-08-05 23:35:35 +0700

    +runclick done

---
+++ Assets/Scripts/UiManagerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManagerController : MonoBehaviour
+{
+     // Start is called before the first frame update
+     public Button bag, attack, run, pick;
+     public GameObject human;
+
+     private Animator ator;
+     void Start()
+     {
+          ator = human.GetComponent<Animator>();
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }
+     public void RunClick()
+     {
+          run.GetComponent<Image>().color = !ator.GetBool("run") ? Color.red : Color.white;
+          ator.SetBool("run", !ator.GetBool("run"));
+     }
+     public void BagClick()
+     {
+
+     }
+     public void PickClick()
+     {
+
+     }
+     public void AttackClick()
+     {
+          
+     }
+}
