21764936661aa1166290062d46ecc1e0dbd90d5e
TienNguyen <ngoctiennguyen155@gmail.com>
2020-08-05 15:16:11 +0700

    +add character and animations to map

--- Assets/Scripts/ThirdPersonUserControl.cs
+++ Assets/Scripts/ThirdPersonUserControl.cs
@@ -5,112 +5,112 @@ using UnityEngine.EventSystems;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-	
-	[RequireComponent(typeof (ThirdPersonCharacter))]
-	public class ThirdPersonUserControl : MonoBehaviour
-	{
-		public GameObject interactionCircle;
-		public float interactionRadius = 0.5f;
-		private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-		private Transform m_Cam;                  // A reference to the main camera in the scenes transform
-		private Vector3 m_CamForward;             // The current forward direction of the camera
-		private Vector3 m_Move;
-		private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 
-		
-		private void Start()
-		{
-			Cursor.lockState = CursorLockMode.Locked;
-			// get the transform of the main camera
-			if (Camera.main != null)
-			{
-				m_Cam = Camera.main.transform;
-			}
-			else
-			{
-				Debug.LogWarning(
-					"Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-				// we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-			}
-
-			// get the third person character ( this should never be null due to require component )
-			m_Character = GetComponent<ThirdPersonCharacter>();
-		}
+     [RequireComponent(typeof(ThirdPersonCharacter))]
+     public class ThirdPersonUserControl : MonoBehaviour
+     {
+          public GameObject interactionCircle;
+          public float interactionRadius = 0.5f;
+          private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+          private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+          private Vector3 m_CamForward;             // The current forward direction of the camera
+          private Vector3 m_Move;
+          private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 
 
-		private void Update()
-		{
-			if (!m_Jump)
-			{
-				m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-			}
-		}
+          private void Start()
+          {
+               Cursor.lockState = CursorLockMode.Locked;
+               // get the transform of the main camera
+               if (Camera.main != null)
+               {
+                    m_Cam = Camera.main.transform;
+               }
+               else
+               {
+                    Debug.LogWarning(
+                         "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+                    // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+               }
+
+               // get the third person character ( this should never be null due to require component )
+               m_Character = GetComponent<ThirdPersonCharacter>();
+          }
 
 
-		// Fixed update is called in sync with physics
-		private void FixedUpdate()
-		{
-			/*if(EventSystem.current.IsPointerOverGameObject())
+          private void Update()
+          {
+               if (!m_Jump)
+               {
+                    m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+               }
+          }
+
+
+          // Fixed update is called in sync with physics
+          private void FixedUpdate()
+          {
+               /*if(EventSystem.current.IsPointerOverGameObject())
             {
 				return;
             }*/
-			// read inputs
-			float h = CrossPlatformInputManager.GetAxis("Horizontal");
-			float v = CrossPlatformInputManager.GetAxis("Vertical");
-			bool crouch = Input.GetKey(KeyCode.C);
+               // read inputs
+               float h = CrossPlatformInputManager.GetAxis("Horizontal");
+               float v = CrossPlatformInputManager.GetAxis("Vertical");
+               bool crouch = Input.GetKey(KeyCode.C);
 
-			// calculate move direction to pass to character
-			if (m_Cam != null)
-			{
-				// calculate camera relative direction to move:
-				m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-				m_Move = v*m_CamForward + h*m_Cam.right;
-			}
-			else
-			{
-				// we use world-relative directions in the case of no main camera
-				m_Move = v*Vector3.forward + h*Vector3.right;
-			}
+               // calculate move direction to pass to character
+               if (m_Cam != null)
+               {
+                    // calculate camera relative direction to move:
+                    m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+                    m_Move = v * m_CamForward + h * m_Cam.right;
+               }
+               else
+               {
+                    // we use world-relative directions in the case of no main camera
+                    m_Move = v * Vector3.forward + h * Vector3.right;
+               }
 #if !MOBILE_INPUT
-			// walk speed multiplier
-			if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+               // walk speed multiplier
+               if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
 #endif
 
-			// pass all parameters to the character control script
-			m_Character.Move(m_Move, crouch, m_Jump);
-			m_Jump = false;
+               // pass all parameters to the character control script
+               m_Character.Move(m_Move, crouch, m_Jump);
+               m_Jump = false;
 
-			Interactable itemFound = CheckItemAround();
+               Interactable itemFound = CheckItemAround();
 
-			if (itemFound != null)
-            {
-                if (Input.GetKeyDown(KeyCode.E))
-				{
-					interactionCircle.SetActive(false);
-					itemFound.Interact();
-                }
-            }
-		}
+               if (itemFound != null)
+               {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                         interactionCircle.SetActive(false);
+                         itemFound.Interact();
+                    }
+               }
+          }
 
-		Interactable CheckItemAround()
-		{
-			Collider[] hits = Physics.OverlapSphere(transform.position, interactionRadius);
-			foreach(Collider hit in hits)
-            {
-				Interactable interactable = hit.GetComponent<Interactable>();
-				if (interactable != null)
-				{
-					interactionCircle.transform.position = new Vector3(hit.transform.position.x, 0.005f, hit.transform.position.z);
-					interactionCircle.SetActive(true);
-					return interactable;
-				}
-			}
+          Interactable CheckItemAround()
+          {
+               Collider[] hits = Physics.OverlapSphere(transform.position, interactionRadius);
+               foreach (Collider hit in hits)
+               {
+                    Interactable interactable = hit.GetComponent<Interactable>();
+                    if (interactable != null)
+                    {
+                         interactionCircle.transform.position = new Vector3(hit.transform.position.x, 0.005f, hit.transform.position.z);
+                         interactionCircle.SetActive(true);
+                         return interactable;
+                    }
+               }
 
-			if (interactionCircle.activeSelf)
-			{
-				interactionCircle.SetActive(false);
-			}
-			return null;
-		}
-	}
+               if (interactionCircle.activeSelf)
+               {
+                    interactionCircle.SetActive(false);
+               }
+               return null;
+          }
+     }
 }
