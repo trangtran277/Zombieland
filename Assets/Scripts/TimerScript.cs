b96038c787f572fdd73b5540175dbd59aeff2a15
trangtran277 <trang-tran277@outlook.com>
2020-08-05 23:32:31 +0700

    add countdown timer

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -5,21 +5,29 @@ using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
-    public Text timerText;
+    [SerializeField] Text timerText;
+    [SerializeField] float startTime = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startTime *= 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float timer = Time.timeSinceLevelLoad;
-        int minute = (int)timer / 60;
-        int sec = (int)timer % 60;
-        string displayMin = minute < 10 ? "0" + minute : "" + minute;
-        string displaySec = sec < 10 ? "0" + sec : "" + sec;
-        timerText.text = displayMin + " : " + displaySec;
+        float timer = startTime - Time.timeSinceLevelLoad;
+        if(timer > 0)
+        {
+            int minute = (int)timer / 60;
+            int sec = (int)timer % 60;
+            string displayMin = minute < 10 ? "0" + minute : "" + minute;
+            string displaySec = sec < 10 ? "0" + sec : "" + sec;
+            timerText.text = displayMin + " : " + displaySec;
+        }
+        else
+        {
+            timerText.GetComponent<Text>().color = Color.red;
+        }
     }
 }
