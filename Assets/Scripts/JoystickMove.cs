4a6303102f741d76cea04457cc6fea4be694901a
TienNguyen <ngoctiennguyen155@gmail.com>
2020-08-05 21:25:41 +0700

    testscriptjoystick

--- Assets/Scripts/JoystickMove.cs
+++ Assets/Scripts/JoystickMove.cs
@@ -15,13 +15,8 @@ public class JoystickMove : MonoBehaviour
      {
 
      }
-     //  void Update()
-     //  {
-     //       Debug.Log(joystick.Horizontal);
-     //       Debug.Log(joystick.Vertical);
-     //       playMakerFSM.FsmVariables.GetFsmFloat("Input mag").Value = Mathf.Abs(Mathf.Max(joystick.Horizontal, joystick.Vertical));
-     //       playMakerFSM.FsmVariables.GetFsmVector3("Input vector").Value = new Vector3(joystick.Horizontal, 0f, joystick.Vertical);
-     //       Debug.Log(playMakerFSM.FsmVariables.GetFsmFloat("Input mag").Value);
-     //       Debug.Log(playMakerFSM.FsmVariables.GetFsmVector3("Input vector").Value);
-     //  }
+     void Update()
+    {
+          Debug.Log(joystick.Vertical);
+     }
 }
