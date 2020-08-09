65f4377a17939d2118e59b69b31c6a286f1930d5
HoaiPham187 <66927484+HoaiPham187@users.noreply.github.com>
2020-08-09 17:48:07 +0700

    Adjust player control

--- Assets/Scripts/FreeLookCam.cs
+++ Assets/Scripts/FreeLookCam.cs
@@ -27,6 +27,7 @@ namespace UnityStandardAssets.Cameras
 		private Vector3 m_PivotEulers;
 		private Quaternion m_PivotTargetRot;
 		private Quaternion m_TransformTargetRot;
+        public FixedTouchField fixedTouchField;
 
         protected override void Awake()
         {
@@ -73,8 +74,10 @@ namespace UnityStandardAssets.Cameras
 			return;
 
             // Read the user input
-            var x = CrossPlatformInputManager.GetAxis("Mouse X");
-            var y = CrossPlatformInputManager.GetAxis("Mouse Y");
+            //var x = CrossPlatformInputManager.GetAxis("Mouse X");
+            //var y = CrossPlatformInputManager.GetAxis("Mouse Y");
+            var x = fixedTouchField.TouchDist.x;
+            var y = fixedTouchField.TouchDist.y;
 
             // Adjust the look angle by an amount proportional to the turn speed and horizontal input.
             m_LookAngle += x*m_TurnSpeed;
