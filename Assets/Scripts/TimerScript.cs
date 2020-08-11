9a2d7b84e26e916a51b00f447dbd1abbd6c48b4f
trangtran277 <trangtran.1997@gmail.com>
2020-08-11 16:30:06 +0700

    enemy spawn when reach a time

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -6,40 +6,38 @@ using UnityEngine.UI;
 public class TimerScript : MonoBehaviour
 {
     [SerializeField] Text timerText;
-    [SerializeField] float startTime = 10f;
+    [SerializeField] float startNightTime = 10f;
     [SerializeField] Transform player;
     [SerializeField] GameObject enemy;
     [SerializeField] float spawnNumber = 10f;
     [SerializeField] Transform centerPos;
     [SerializeField] float radius;
+
     private bool hasSpawned = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        startTime *= 60;
+        startNightTime *= 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float timer = startTime - Time.timeSinceLevelLoad;
-        if(timer > 0)
-        {
-            int minute = (int)timer / 60;
-            int sec = (int)timer % 60;
-            string displayMin = minute < 10 ? "0" + minute : "" + minute;
-            string displaySec = sec < 10 ? "0" + sec : "" + sec;
-            timerText.text = displayMin + " : " + displaySec;
-        }
-        else
+        float timer = Time.timeSinceLevelLoad;
+        int minute = (int)timer / 60;
+        int sec = (int)timer % 60;
+        string displayMin = minute < 10 ? "0" + minute : "" + minute;
+        string displaySec = sec < 10 ? "0" + sec : "" + sec;
+        timerText.text = displayMin + " : " + displaySec;
+
+        if(timer >= startNightTime)
         {
             if(!hasSpawned)
             {
                 Spawn();
                 hasSpawned = true;
             }
-            timerText.GetComponent<Text>().color = Color.red;
         }
     }
     public void Spawn()
