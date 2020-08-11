01d6412ab2e0ae75c5c9db9075569706384dec8b
HoaiPham187 <66927484+HoaiPham187@users.noreply.github.com>
2020-08-11 12:22:36 +0700

    Added Notes system and UI

---
+++ Assets/Scripts/NotesUIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotesUIManager : MonoBehaviour
+{
+    #region Singleton
+    public static NotesUIManager instance;
+    private void Awake()
+    {
+        if (instance != null) return;
+        instance = this;
+    }
+    #endregion
+    public GameObject noteReadingUI;
+    public Text readingContent;
+    public LayoutGroup noteListUI;
+    public GameObject noteButton;
+    NotesManager notesManager;
+    int curCount = -1;
+
+    private void Start()
+    {
+        notesManager = NotesManager.instance;
+    }
+    public void UpdateUI()
+    {
+        for(int i = 0; i <= notesManager.notesList.Count - 1; i++)
+        {
+            if(i > curCount)
+            {
+                GameObject newButton = Instantiate(noteButton, noteListUI.gameObject.transform);
+                newButton.transform.SetAsFirstSibling();
+                newButton.GetComponentInChildren<Text>().text = notesManager.notesList[i].title;
+                newButton.GetComponent<NoteButton>().note = notesManager.notesList[i];
+                curCount++;
+            }
+        }
+    }
+}
