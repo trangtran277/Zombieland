9bddc5a360b694a5115a63fe4a68ac1ddf580dc6
HoaiPham187 <66927484+HoaiPham187@users.noreply.github.com>
2020-08-17 18:28:05 +0700

    Fix outline issue

---
+++ Assets/Scripts/ToggleOutline.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleOutline : MonoBehaviour
+{
+    cakeslice.Outline outline;
+    float maxRange = 10f;
+    RaycastHit hit;
+
+    private void Start()
+    {
+        outline = GetComponent<cakeslice.Outline>();
+    }
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, Camera.main.transform.position) < maxRange)
+        {
+            if (Physics.Raycast(transform.position, (Camera.main.transform.position - transform.position), out hit, maxRange))
+            {
+                Debug.DrawLine(transform.position, Camera.main.transform.position, Color.red);
+                if (hit.transform.CompareTag("Camera"))
+                {
+                    outline.enabled = true;
+                }
+                else
+                {
+                    outline.enabled = false;
+                }
+            }
+        }
+        else
+        {
+            outline.enabled = false;
+        }
+    }
+}
