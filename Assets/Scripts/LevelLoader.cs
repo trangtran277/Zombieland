24ab64fb3cdd605a0c8da51a53f753215cd66773
trangtran277 <trangtran.1997@gmail.com>
2020-08-26 11:36:13 +0700

    level load with debug log

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject mainMenuUI;
+    public GameObject loadingUI;
+    public void LoadLevel()
+    {
+        Debug.Log("Attempt to load level?");
+        //mainMenuUI.SetActive(false);
+        //loadingUI.SetActive(true);
+        StartCoroutine("LoadAsynchronously");
+    }
+
+    IEnumerator LoadAsynchronously()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Running coroutine");
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movie : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        var vp = GetComponent<UnityEngine.Video.VideoPlayer>();
+        vp.Play();
+    }
+}
